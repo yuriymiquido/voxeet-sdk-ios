722edb816f6757db727469901c826122cd677f44
Yuriy Ganushevich <yuriy.ganushevich@miquido.com>
2022-01-01 15:18:55 +0100

    Update Package.swift file

--- Package.swift
+++ Package.swift
@@ -12,7 +12,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "VoxeetSDK",
-            targets: ["VoxeetSDK", "WebRTC", "dvclient"]),
+            targets: [
+                "VoxeetSDK", 
+                "WebRTC", 
+                "dvclient", 
+            ]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
